737315f00ca40a56eb2c582c930db0497ffb2ebe
MarkoMarinovicc <marko.marinovic@playhera.com>
2023-06-10 16:07:45 +0200

    exercise 1.16*

--- unicafe/unicafe/src/Statistics.jsx
+++ unicafe/unicafe/src/Statistics.jsx
@@ -6,15 +6,22 @@ const Statistics = ({ good, neutral, bad, all, average, positive }) => {
     return <p>No feedback given</p>;
   }
   return (
-    <div>
+    <>
       <h1>statistics</h1>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="netural" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average === 0 ? 0 : average} />
-      <StatisticLine text="positive" value={positive === 0 ? 0 : positive} />
-    </div>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="netural" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average === 0 ? 0 : average} />
+          <StatisticLine
+            text="positive"
+            value={positive === 0 ? 0 : positive}
+          />
+        </tbody>
+      </table>
+    </>
   );
 };
 
