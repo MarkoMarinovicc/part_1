d17a94171fd59315f0825e94da6c113a24542ad4
MarkoMarinovicc <marko.marinovic@playhera.com>
2023-06-10 10:04:04 +0200

    exercise 1.7

--- unicafe/unicafe/src/App.js
+++ unicafe/unicafe/src/App.js
@@ -5,7 +5,9 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
+  const all = good + neutral + bad;
+  const average = (good - bad) / all || 0;
+  const positive = (good / all) * 100;
   return (
     <div>
       <h1>give feedback</h1>
@@ -13,9 +15,12 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>statistics</h1>
-      <span>good {good}</span>
-      <span> neutral {neutral}</span>
-      <span> bad {bad}</span>
+      <p>good {good}</p>
+      <p> neutral {neutral}</p>
+      <p> bad {bad}</p>
+      <p>all {all}</p>
+      <p>average{average === 0 ? "0" : average} </p>
+      <p>positive {positive}%</p>
     </div>
   );
 };
